51000f9d706fb3e6a09cbe08b7ebad1974a1965e
Brian7950 <brianh7950@gmail.com>
2022-04-04 23:20:21 -0600

    added player methods and tests

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -3,16 +3,16 @@ const Player = require('../lib/Player.js');
 const Potion = require('../lib/Potion');
 jest.mock('../lib/Potion');
 
-test('creates a player object' , () =>{
+test('creates a player object', () => {
     const player = new Player('Dave');
 
     expect(player.name).toBe('Dave');
     expect(player.health).toEqual(expect.any(Number));
     expect(player.strength).toEqual(expect.any(Number));
-  expect(player.agility).toEqual(expect.any(Number));
-  expect(player.inventory).toEqual(
-    expect.arrayContaining([expect.any(Object)])
-  );
+    expect(player.agility).toEqual(expect.any(Number));
+    expect(player.inventory).toEqual(
+        expect.arrayContaining([expect.any(Object)])
+    );
 });
 
 test("gets player's stats as an object", () => {
@@ -22,14 +22,42 @@ test("gets player's stats as an object", () => {
     expect(player.getStats()).toHaveProperty('health');
     expect(player.getStats()).toHaveProperty('strength');
     expect(player.getStats()).toHaveProperty('agility');
-  });
+});
 
-  test('gets inventory from player or returns false', () => {
+test('gets inventory from player or returns false', () => {
     const player = new Player('Dave');
-  
+
     expect(player.getInventory()).toEqual(expect.any(Array));
-  
+
     player.inventory = [];
-  
+
     expect(player.getInventory()).toEqual(false);
-  })
+});
+
+test("gets player's healt value", () => {
+    const player = new Player('Dave');
+
+    expect(player.getHealth()).toEqual(expect.stringContaining(player.health.toString()));
+});
+
+test('checks to see if player is alive or not', () =>{
+    const player = new Player('Dave');
+
+    expect(player.isAlive()).toBeTruthy();
+    player.health = 0;
+    expect(player.isAlive()).toBeFalsy();
+})
+
+
+test("subtracts from player's health", () => {
+    const player = new Player('Dave');
+    const oldHealth = player.health;
+  
+    player.reduceHealth(5);
+  
+    expect(player.health).toBe(oldHealth - 5);
+  
+    player.reduceHealth(99999);
+  
+    expect(player.health).toBe(0);
+  });
